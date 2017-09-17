61759825ace157ba4b8c6d199702b54c2c5c7740
RockingDice <wonderplay@outlook.com>
2017-09-17 23:27:38 +0800

    fix igv integration issue

--- AutoCategory_Integrations_Inventory_Grid_View.lua
+++ AutoCategory_Integrations_Inventory_Grid_View.lua
@@ -550,21 +550,28 @@ end
 ---------------------------------------------------------------------------
 local integrated = false
 function IntegrateInventoryGridView() 
-	if integrated == false and InventoryGridView ~= nil then		
-		d("integrated with igv")
-		IGV = InventoryGridView
-		util = IGV.util
-		settings = IGV.settings
-		adapter = IGV.adapter
-		--integrate
-		InventoryGridView.adapter.ScrollController = adapter_ScrollController
-		
-		InventoryGridView.adapter.ToggleGrid = adapter_ToggleGrid
-		
-		InventoryGridView.util.ReshapeSlots = util_ReshapeSlots 
-		integrated = true
-	end
+	zo_callLater(function() 
+		if integrated == false and InventoryGridView ~= nil then		
+			IGV = InventoryGridView
+			util = IGV.util
+			settings = IGV.settings
+			adapter = IGV.adapter
+			--integrate
+			InventoryGridView.adapter.ScrollController = adapter_ScrollController
+			
+			InventoryGridView.adapter.ToggleGrid = adapter_ToggleGrid
+			
+			InventoryGridView.util.ReshapeSlots = util_ReshapeSlots 
+			 
+			integrated = true
+			
+			--fix prehook  	
+			ZO_PreHook("ZO_ScrollList_UpdateScroll", adapter_ScrollController)
+		end
+	end, 10) 
 end 
 
 EVENT_MANAGER:RegisterForEvent(AutoCategory.name, EVENT_PLAYER_ACTIVATED, IntegrateInventoryGridView)
+
+		
 ------------------------modified end--------------------------
