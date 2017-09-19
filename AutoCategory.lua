5adfb4917b2f2148d06324fa664983bf1d5122fd
RockingDice <wonderplay@outlook.com>
2017-09-19 11:05:27 +0800

    update version to 1.08

--- AutoCategory.lua
+++ AutoCategory.lua
@@ -15,7 +15,7 @@ AutoCategory.RuleFunc = {}
 AutoCategory.Inited = false
 
 AutoCategory.name = "AutoCategory";
-AutoCategory.version = "1.07";
+AutoCategory.version = "1.08";
 AutoCategory.settingName = "Auto Category"
 AutoCategory.settingDisplayName = "RockingDice's AutoCategory"
 AutoCategory.author = "RockingDice"
